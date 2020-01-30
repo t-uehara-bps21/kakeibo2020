ef933af59a6ece58a6f38f0f503276c4cfac034c
t-uehara-bps21 <60417732+t-uehara-bps21@users.noreply.github.com>
2020-01-30 15:01:40 +0900

    update 最終データ

--- ランキング.vb
+++ ランキング.vb
@@ -4,13 +4,11 @@ Private Type Hairetsu
 End Type
 
 Sub ランキング()
+
     Dim arr() As Hairetsu
+    Dim val As Hairetsu
     Dim i As Long
     Dim j As Long
-    Dim vmax As Long
-    Dim val As Long
-    Dim ym As Long
-    Dim ym2 As Long
     Dim tsukist As Worksheet
     Dim datast As Worksheet
     Dim senntaku As String
@@ -19,55 +17,43 @@ Sub ランキング()
     Set datast = Sheets("データ")
     
     For i = 1 To Rows().Count
-        
+    
     ReDim Preserve arr(i)
         If tsukist.Cells(3 + i, "D") = "" Then
-            Exit For
-            
+            Exit For  
         End If
         
-        arr(i) = tsukist.Cells(3 + i, "C") 
+        arr(i).ym = tsukist.Cells(3 + i, "C")
         senntaku = datast.Cells(16, "B")
-
+         
         Select Case senntaku
             Case "収支"
-                arr(i).money = tsukist.Cells(3 + i, "D")
+                arr(i).money = tsukist.Cells(3 + i, "D")   
             Case "収入"
                 arr(i).money = tsukist.Cells(3 + i, "E")
             Case "支出"
                 arr(i).money = tsukist.Cells(3 + i, "F")
             Case "貯蓄"
                 arr(i).money = tsukist.Cells(3 + i, "G")
-        End Select
-        
+            End Select
     Next i
-    
+  
     For i = 1 To UBound(arr) - 1
-        vmax = arr(i)
-        ym2 = arr(i)
-        
         For j = 1 To UBound(arr) - 1
-            val = arr(j)
-            ym = arr(j)
         
-        If vmax > val Then
-            arr(j) = vmax
-            arr(i) = val
-            vmax = val
-            
-            arr(j) = ym2
-            arr(i) = ym
-            ym2 = ym
-            
-        End If
+            If arr(i).money > arr(j).money Then
+                val = arr(i)
+                arr(i) = arr(j)
+                arr(j) = val
+            End If
         Next j
     Next i
-               
-    datast.Cells(16, "F") = arr(1)
-    datast.Cells(17, "F") = arr(2)
-    datast.Cells(18, "F") = arr(3)
-    datast.Cells(16, "E") = arr(1)
-    datast.Cells(17, "E") = arr(2)
-    datast.Cells(18, "E") = arr(3)
- 
+  
+    datast.Cells(16, "F") = arr(1).money
+    datast.Cells(17, "F") = arr(2).money
+    datast.Cells(18, "F") = arr(3).money
+    datast.Cells(16, "E") = arr(1).ym
+    datast.Cells(17, "E") = arr(2).ym
+    datast.Cells(18, "E") = arr(3).ym
+  
 End Sub
