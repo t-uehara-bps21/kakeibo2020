7c8c7cecbe740a5cdfe73827caf2ddaa87ee5f84
t-uehara-bps21 <60417732+t-uehara-bps21@users.noreply.github.com>
2020-01-30 16:42:32 +0900

    update 修正

--- ランキング.vb
+++ ランキング.vb
@@ -1,59 +1,54 @@
-Private Type Hairetsu
-    money As Long
-    ym As Long
-End Type
+Sub 月別ランキング()
 
-Sub ランキング()
-
-    Dim arr() As Hairetsu
-    Dim val As Hairetsu
+    Dim RankArr() As Arr
+    Dim Val As Arr
     Dim i As Long
     Dim j As Long
-    Dim tsukist As Worksheet
-    Dim datast As Worksheet
-    Dim senntaku As String
+    Dim MonthTotalSt As Worksheet
+    Dim DataSt As Worksheet
+    Dim Senntaku As String
     
-    Set tsukist = Sheets("月別合計")
-    Set datast = Sheets("データ")
+    Set MonthTotalSt = Sheets("月別合計")
+    Set DataSt = Sheets("データ")
     
     For i = 1 To Rows().Count
-    
-    ReDim Preserve arr(i)
-        If tsukist.Cells(3 + i, "D") = "" Then
-            Exit For  
-        End If
+        ReDim Preserve RankArr(i)
         
-        arr(i).ym = tsukist.Cells(3 + i, "C")
-        senntaku = datast.Cells(16, "B")
+            If MonthTotalSt.Cells(3 + i, "D") = "" Then
+                Exit For
+            End If
+        
+            RankArr(i).Month = MonthTotalSt.Cells(3 + i, "C")
+            Senntaku = DataSt.Cells(16, "B")
          
-        Select Case senntaku
-            Case "収支"
-                arr(i).money = tsukist.Cells(3 + i, "D")   
-            Case "収入"
-                arr(i).money = tsukist.Cells(3 + i, "E")
-            Case "支出"
-                arr(i).money = tsukist.Cells(3 + i, "F")
-            Case "貯蓄"
-                arr(i).money = tsukist.Cells(3 + i, "G")
-            End Select
+            Select Case Senntaku
+                Case "収支"
+                    RankArr(i).TotalValue = MonthTotalSt.Cells(3 + i, "D")
+                Case "収入"
+                    RankArr(i).TotalValue = MonthTotalSt.Cells(3 + i, "E")
+                Case "支出"
+                    RankArr(i).TotalValue = MonthTotalSt.Cells(3 + i, "F")
+                Case "貯蓄"
+                    RankArr(i).TotalValue = MonthTotalSt.Cells(3 + i, "G")
+                End Select
     Next i
   
-    For i = 1 To UBound(arr) - 1
-        For j = 1 To UBound(arr) - 1
+    For i = 1 To UBound(RankArr) - 1
+        For j = 1 To UBound(RankArr) - 1
         
-            If arr(i).money > arr(j).money Then
-                val = arr(i)
-                arr(i) = arr(j)
-                arr(j) = val
-            End If
+        If RankArr(i).TotalValue > RankArr(j).TotalValue Then
+            Val = RankArr(i)
+            RankArr(i) = RankArr(j)
+            RankArr(j) = Val 
+        End If
         Next j
     Next i
   
-    datast.Cells(16, "F") = arr(1).money
-    datast.Cells(17, "F") = arr(2).money
-    datast.Cells(18, "F") = arr(3).money
-    datast.Cells(16, "E") = arr(1).ym
-    datast.Cells(17, "E") = arr(2).ym
-    datast.Cells(18, "E") = arr(3).ym
+    DataSt.Cells(16, "F") = RankArr(1).TotalValue
+    DataSt.Cells(17, "F") = RankArr(2).TotalValue
+    DataSt.Cells(18, "F") = RankArr(3).TotalValue
+    DataSt.Cells(16, "E") = RankArr(1).Month
+    DataSt.Cells(17, "E") = RankArr(2).Month
+    DataSt.Cells(18, "E") = RankArr(3).Month
   
 End Sub
